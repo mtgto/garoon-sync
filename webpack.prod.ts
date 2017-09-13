b76096572b713ac23fa4c2cf71dbf30dd78d094d
mtgto <hogerappa@gmail.com>
2017-09-14 01:12:10 +0900

    Use uglify in production build.

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,6 +1,7 @@
 import * as webpack from "webpack";
 import * as merge from "webpack-merge";
 import {renderConfig, mainConfig} from "./webpack.common";
+const UglifyPlugin = require("uglifyjs-webpack-plugin");
 
 export default [
     merge(renderConfig, {
@@ -10,6 +11,7 @@ export default [
                     'NODE_ENV': JSON.stringify('production')
                 }
             }),
+            new UglifyPlugin()
         ]
     }),
     merge(mainConfig, {
@@ -19,6 +21,7 @@ export default [
                     'NODE_ENV': JSON.stringify('production')
                 }
             }),
+            new UglifyPlugin()
         ]
     })
 ];
